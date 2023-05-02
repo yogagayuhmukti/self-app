4756be39adba07405b2b745c0607623f8d4ead28
gm906~ <gayuhmukti906@gmail.com>
2023-05-02 11:35:09 +0700

    linked

--- src/pages/LandingPage/YogaIntroduce/YogaIntroduce.jsx
+++ src/pages/LandingPage/YogaIntroduce/YogaIntroduce.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import {
   yoga,
   yogabackground2,
@@ -8,8 +8,14 @@ import {
   whatsapp,
 } from "../../../assets";
 import Button from "react-bootstrap/Button";
+import ReactWhatsapp from "react-whatsapp";
+import Overlay from "react-bootstrap/Overlay";
+import Tooltip from "react-bootstrap/Tooltip";
 
 const YogaIntroduce = () => {
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+
   return (
     <>
       <div className="yogaIntroduce">
@@ -58,10 +64,33 @@ const YogaIntroduce = () => {
               <Button variant="success">Portofolio</Button>
             </div>
             <div className="yogalink">
-              <img className="yogalinkpic" src={email} alt="email" />
-              <img className="yogalinkpic" src={facebook} alt="facebook" />
-              <img className="yogalinkpic" src={instagram} alt="instagram" />
-              <img className="yogalinkpic" src={whatsapp} alt="whatsapp" />
+              <Button
+                className="buttonmail"
+                ref={target}
+                onClick={() => setShow(!show)}
+              >
+                <img className="yogalinkpic" src={email} alt="email" />
+              </Button>
+              <Overlay target={target.current} show={show} placement="right">
+                {(props) => (
+                  <Tooltip id="overlay-example" {...props}>
+                    ygmukti@gmail.com
+                  </Tooltip>
+                )}
+              </Overlay>
+              <a href="https://www.facebook.com/yogagayuh.mukti?mibextid=ZbWKwL">
+                <img className="yogalinkpic" src={facebook} alt="facebook" />
+              </a>
+              <a href="https://instagram.com/gmstudi0_?igshid=ZDdkNTZiNTM= ">
+                <img className="yogalinkpic" src={instagram} alt="instagram" />
+              </a>
+              <ReactWhatsapp
+                className="whatsappbutton"
+                number="=+6285326721633"
+                message="hello"
+              >
+                <img className="yogalinkpic" src={whatsapp} alt="whatsapp" />
+              </ReactWhatsapp>
             </div>
           </div>
         </div>
