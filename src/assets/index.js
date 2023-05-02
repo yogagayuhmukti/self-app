021d22fedfb4b2fb0c872f164cc755098bf1cb96
gm906~ <gayuhmukti906@gmail.com>
2023-05-02 10:08:43 +0700

    fixed

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,19 @@ import yoga from "./yoga.png";
 import yogabackground from "./background.jpg";
 import yogabackground2 from "./background2.jpg";
 import yogaintro from "./yogaintro.gif";
+import email from "./email.png";
+import facebook from "./facebook.png";
+import instagram from "./instagram.png";
+import whatsapp from "./whatsapp.png";
 
-export { welcome, yoga, yogabackground, yogabackground2, yogaintro };
+export {
+  welcome,
+  yoga,
+  yogabackground,
+  yogabackground2,
+  yogaintro,
+  email,
+  facebook,
+  instagram,
+  whatsapp,
+};
--- src/pages/LandingPage/YogaIntroduce/YogaIntroduce.jsx
+++ src/pages/LandingPage/YogaIntroduce/YogaIntroduce.jsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { yoga, yogabackground2 } from "../../../assets";
+import {
+  yoga,
+  yogabackground2,
+  email,
+  facebook,
+  instagram,
+  whatsapp,
+} from "../../../assets";
 import Button from "react-bootstrap/Button";
 
 const YogaIntroduce = () => {
@@ -15,37 +22,46 @@ const YogaIntroduce = () => {
           <div>
             <div className="yogaprofile">
               <img className="mobileintropic" src={yoga} alt="yoga" />
-              <div>
+              <div className="yogaprifileintro">
                 <h3>Hi, I am</h3>
                 <h1>Yoga Gayuh Mukti</h1>
                 <h4>Frontend Web Developer</h4>
               </div>
             </div>
-            <p className="desktopintro">
-              I am an enthusiastic and committed programmer with a deep passion
-              for technology. I am seeking to join a company that values ethics
-              and maintains high standards. As a good communicator and team
-              player, I enjoy collaborating with others and contributing to a
-              shared vision of excellence. I am highly adaptable and always
-              eager to learn new skills and gain new experiences. My dedication
-              to programming and technology is a driving force in my life, and I
-              am committed to pursuing my passion with hard work, enthusiasm,
-              and a commitment to excellence.
-            </p>
-            <p className="mobileintro">
-              I'm a passionate programmer looking for a company with good ethics
-              and high standards. I'm a team player who communicates well and
-              strives for excellence. I'm adaptable, eager to learn, and
-              dedicated to programming and technology.
-            </p>
-            <p>
-              <strong>Address</strong> Klapagading RT02/03, Kecamatan Wangon,
-              Kabupaten Banyumas, Jawa Tengah, Indonesia
-            </p>
+            <div>
+              <p className="desktopintro">
+                I am an enthusiastic and committed programmer with a deep
+                passion for technology. I am seeking to join a company that
+                values ethics and maintains high standards. As a good
+                communicator and team player, I enjoy collaborating with others
+                and contributing to a shared vision of excellence. I am highly
+                adaptable and always eager to learn new skills and gain new
+                experiences. My dedication to programming and technology is a
+                driving force in my life, and I am committed to pursuing my
+                passion with hard work, enthusiasm, and a commitment to
+                excellence.
+              </p>
+              <p className="mobileintro">
+                I'm a passionate programmer looking for a company with good
+                ethics and high standards. I'm a team player who communicates
+                well and strives for excellence. I'm adaptable, eager to learn,
+                and dedicated to programming and technology.
+              </p>
+              <p>
+                <strong>Address</strong> Klapagading RT02/03, Kecamatan Wangon,
+                Kabupaten Banyumas, Jawa Tengah, Indonesia
+              </p>
+            </div>
             <div className="yogabutton">
               <Button variant="success">Curriculum Vitae</Button>
               <br></br>
               <Button variant="success">Portofolio</Button>
+            </div>
+            <div className="yogalink">
+              <img className="yogalinkpic" src={email} alt="email" />
+              <img className="yogalinkpic" src={facebook} alt="facebook" />
+              <img className="yogalinkpic" src={instagram} alt="instagram" />
+              <img className="yogalinkpic" src={whatsapp} alt="whatsapp" />
             </div>
           </div>
         </div>
