2a394f338d740fea65376b8787a28704774aa15c
gm906~ <gayuhmukti906@gmail.com>
2023-05-03 13:47:04 +0700

    portofolio

--- src/pages/Portofolio/Portofolio.jsx
+++ src/pages/Portofolio/Portofolio.jsx
@@ -8,37 +8,39 @@ const Portofolio = () => {
   return (
     <>
       <Parallax
-        className="backgroundparallax"
+        className="backgroundportofolio"
         bgImage={yogabackground}
         bgImageAlt="yogabackground"
-        strength={200}
+        strength={300}
       >
         <div className="welcomeportofolio">Welcome to my Portofolio</div>
       </Parallax>
-      <div className="listjudul">
-        <div className="judul">Binar Cademy as User</div>
-        <a href="https://bcr-platinum-user.vercel.app/">
-          <Button variant="success"> See More Detail </Button>
-        </a>
+      <div className="portocontainer">
+        <div className="listjudul">
+          <div className="judul">Binar Cademy as User</div>
+          <a href="https://bcr-platinum-user.vercel.app/">
+            <Button variant="success"> See More Detail </Button>
+          </a>
+        </div>
+        <img
+          className="imgportofolio"
+          src={yogabinaruser}
+          alt="yogabinaruser"
+        />
       </div>
-      <Parallax
-        className="backgroundparallax"
-        bgImage={yogabinaruser}
-        bgImageAlt="yogabinaruser"
-        strength={200}
-      ></Parallax>
-      <div className="listjudul">
-        <div className="judul">Binar Cademy as Admin</div>
-        <a href="https://bcr-platinum-admin.vercel.app/login">
-          <Button variant="success"> See More Detail </Button>
-        </a>
+      <div className="portocontainer">
+        <div className="listjudul">
+          <div className="judul">Binar Cademy as Admin</div>
+          <a href="https://bcr-platinum-admin.vercel.app/login">
+            <Button variant="success"> See More Detail </Button>
+          </a>
+        </div>
+        <img
+          className="imgportofolio"
+          src={yogabinaradmin}
+          alt="yogabinaradmin"
+        />
       </div>
-      <Parallax
-        className="backgroundparallax"
-        bgImage={yogabinaradmin}
-        bgImageAlt="yogabinaradmin"
-        strength={200}
-      ></Parallax>
     </>
   );
 };
