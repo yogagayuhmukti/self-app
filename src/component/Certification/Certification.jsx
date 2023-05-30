9b44dfadb5876b8a1d641d5e5ac84af4889605df
gm906~ <gayuhmukti906@gmail.com>
2023-05-30 21:22:00 +0700

    menghapus-gambar

--- src/component/Certification/Certification.jsx
+++ src/component/Certification/Certification.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import {
-  sertifikat_binar_academy,
-  sertifikat_dicoding_belajar_dasar_aws_cloud,
-  sertifikat_dicoding_belajar_dasar_dasar_dev_ops,
-  sertifikat_dicoding_belajar_dasar_pemrograman_javascript,
-  sertifikat_dicoding_belajar_dasar_pemrogtraman_web,
-  sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio,
-  sertifikat_dicoding_membuat_frontend_web_untuk_pemula,
-  sertifikat_freecodecamp,
-} from "../../assets";
+// import {
+//   sertifikat_binar_academy,
+//   sertifikat_dicoding_belajar_dasar_aws_cloud,
+//   sertifikat_dicoding_belajar_dasar_dasar_dev_ops,
+//   sertifikat_dicoding_belajar_dasar_pemrograman_javascript,
+//   sertifikat_dicoding_belajar_dasar_pemrogtraman_web,
+//   sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio,
+//   sertifikat_dicoding_membuat_frontend_web_untuk_pemula,
+//   sertifikat_freecodecamp,
+// } from "../../assets";
 
 const Certification = () => {
   return (
@@ -18,62 +18,62 @@ const Certification = () => {
         <ul>
           <h3>2023-05</h3>
           <li>Belajar Dasar AWS Cloud</li>
-          <img
+          {/* <img
             src={sertifikat_dicoding_belajar_dasar_aws_cloud}
             alt="sertifikat_dicoding_belajar_dasar_aws_cloud"
-          />
+          /> */}
         </ul>
         <ul>
           <h3>2023-05</h3>
           <li>Binar Academy Frontend Engineer</li>
-          <img src={sertifikat_binar_academy} alt="sertifikat_binar_academy" />
+          {/* <img src={sertifikat_binar_academy} alt="sertifikat_binar_academy" /> */}
         </ul>
         <ul>
           <h3>2023-04</h3>
           <li>JavaScript Alogarithms and Data Structures.</li>
-          <img src={sertifikat_freecodecamp} alt="sertifikat_freecodecamp" />
+          {/* <img src={sertifikat_freecodecamp} alt="sertifikat_freecodecamp" /> */}
         </ul>
         <ul>
           <h3>2023-03</h3>
           <li>Belajar Dasar Pemrograman Javascript.</li>
-          <img
+          {/* <img
             src={sertifikat_dicoding_belajar_dasar_pemrograman_javascript}
             alt="sertifikat_dicoding_belajar_dasar_pemrograman_javascript"
-          />
+          /> */}
         </ul>
         <ul>
           <h3>2022-11</h3>
           <li>Belajar Dasar-Dasar DevOps.</li>
-          <img
+          {/* <img
             src={sertifikat_dicoding_belajar_dasar_dasar_dev_ops}
             alt="sertifikat_dicoding_belajar_dasar_dasar_dev_ops"
-          />
+          /> */}
         </ul>
         <ul>
           <h3>2022-10</h3>
           <li>Belajar Membuat Augmented Reality dengan Les Studio.</li>
-          <img
+          {/* <img
             src={
               sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio
             }
             alt="sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio"
-          />
+          /> */}
         </ul>
         <ul>
           <h3>2022-09</h3>
           <li>Belajar Membuat Front End Web untuk Pemula.</li>
-          <img
+          {/* <img
             src={sertifikat_dicoding_membuat_frontend_web_untuk_pemula}
             alt="sertifikat_dicoding_membuat_frontend_web_untuk_pemula"
-          />
+          /> */}
         </ul>
         <ul>
           <h3>2022-06</h3>
           <li>Belajar Dasar Pemrograman Web.</li>
-          <img
+          {/* <img
             src={sertifikat_dicoding_belajar_dasar_pemrogtraman_web}
             alt="sertifikat_dicoding_belajar_dasar_pemrogtraman_web"
-          />
+          /> */}
         </ul>
       </div>
     </div>
--- src/component/Language/Language.jsx
+++ src/component/Language/Language.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import {
-  Sertifikat_Bahasa_Inggris,
-  Sertifikat_Bahasa_Korea,
-} from "../../assets";
+// import {
+//   Sertifikat_Bahasa_Inggris,
+//   Sertifikat_Bahasa_Korea,
+// } from "../../assets";
 const Language = () => {
   return (
     <div>
@@ -11,14 +11,14 @@ const Language = () => {
         <h3>English | Korea | Indonesia</h3>
         <div>
           <h5>English certification</h5>
-          <img
+          {/* <img
             src={Sertifikat_Bahasa_Inggris}
             alt="Sertifikat_Bahasa_Inggris"
-          />
+          /> */}
         </div>
         <div>
           <h5>Korean certification</h5>
-          <img src={Sertifikat_Bahasa_Korea} alt="Sertifikat_Bahasa_Korea" />
+          {/* <img src={Sertifikat_Bahasa_Korea} alt="Sertifikat_Bahasa_Korea" /> */}
         </div>
       </ul>
     </div>
