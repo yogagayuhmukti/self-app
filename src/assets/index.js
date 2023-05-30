f8fbf330c3bcd61db105078e15342cb5fd268598
gm906~ <gayuhmukti906@gmail.com>
2023-05-30 07:13:29 +0700

    sertifikat

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,6 +9,14 @@ import instagram from "./instagram.png";
 import whatsapp from "./whatsapp.png";
 import yogabinaruser from "./yogabinaruser.jpg";
 import yogabinaradmin from "./yogabinaradmin.jpg";
+import sertifikat_binar_academy from "./sertifikat_binar_academy.png";
+import sertifikat_dicoding_belajar_dasar_aws_cloud from "./sertifikat_dicoding_belajar_dasar_aws_cloud.png";
+import sertifikat_dicoding_belajar_dasar_dasar_dev_ops from "./sertifikat_dicoding_belajar_dasar_dasar_dev_ops.png";
+import sertifikat_dicoding_belajar_dasar_pemrograman_javascript from "./sertifikat_dicoding_belajar_dasar_pemrograman_javascript.png";
+import sertifikat_dicoding_belajar_dasar_pemrogtraman_web from "./sertifikat_dicoding_belajar_dasar_pemrogtraman_web.png";
+import sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio from "./sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio.png";
+import sertifikat_dicoding_membuat_frontend_web_untuk_pemula from "./sertifikat_dicoding_membuat_frontend_web_untuk_pemula.png";
+import sertifikat_freecodecamp from "./sertifikat_freecodecamp.png";
 
 export {
   welcome,
@@ -22,4 +30,12 @@ export {
   whatsapp,
   yogabinaruser,
   yogabinaradmin,
+  sertifikat_binar_academy,
+  sertifikat_dicoding_belajar_dasar_aws_cloud,
+  sertifikat_dicoding_belajar_dasar_dasar_dev_ops,
+  sertifikat_dicoding_belajar_dasar_pemrograman_javascript,
+  sertifikat_dicoding_belajar_dasar_pemrogtraman_web,
+  sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio,
+  sertifikat_dicoding_membuat_frontend_web_untuk_pemula,
+  sertifikat_freecodecamp,
 };
--- src/component/Certification/Certification.jsx
+++ src/component/Certification/Certification.jsx
@@ -1,4 +1,14 @@
 import React from "react";
+import {
+  sertifikat_binar_academy,
+  sertifikat_dicoding_belajar_dasar_aws_cloud,
+  sertifikat_dicoding_belajar_dasar_dasar_dev_ops,
+  sertifikat_dicoding_belajar_dasar_pemrograman_javascript,
+  sertifikat_dicoding_belajar_dasar_pemrogtraman_web,
+  sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio,
+  sertifikat_dicoding_membuat_frontend_web_untuk_pemula,
+  sertifikat_freecodecamp,
+} from "../../assets";
 
 const Certification = () => {
   return (
@@ -7,31 +17,63 @@ const Certification = () => {
       <div>
         <ul>
           <h3>2023-05</h3>
-          <li> Binar Academy Frontend Engineer</li>
+          <li>Belajar Dasar AWS Cloud</li>
+          <img
+            src={sertifikat_dicoding_belajar_dasar_aws_cloud}
+            alt="sertifikat_dicoding_belajar_dasar_aws_cloud"
+          />
+        </ul>
+        <ul>
+          <h3>2023-05</h3>
+          <li>Binar Academy Frontend Engineer</li>
+          <img src={sertifikat_binar_academy} alt="sertifikat_binar_academy" />
         </ul>
         <ul>
           <h3>2023-04</h3>
           <li>JavaScript Alogarithms and Data Structures.</li>
+          <img src={sertifikat_freecodecamp} alt="sertifikat_freecodecamp" />
         </ul>
         <ul>
           <h3>2023-03</h3>
           <li>Belajar Dasar Pemrograman Javascript.</li>
+          <img
+            src={sertifikat_dicoding_belajar_dasar_pemrograman_javascript}
+            alt="sertifikat_dicoding_belajar_dasar_pemrograman_javascript"
+          />
         </ul>
         <ul>
           <h3>2022-11</h3>
           <li>Belajar Dasar-Dasar DevOps.</li>
+          <img
+            src={sertifikat_dicoding_belajar_dasar_dasar_dev_ops}
+            alt="sertifikat_dicoding_belajar_dasar_dasar_dev_ops"
+          />
         </ul>
         <ul>
           <h3>2022-10</h3>
           <li>Belajar Membuat Augmented Reality dengan Les Studio.</li>
+          <img
+            src={
+              sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio
+            }
+            alt="sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio"
+          />
         </ul>
         <ul>
           <h3>2022-09</h3>
           <li>Belajar Membuat Front End Web untuk Pemula.</li>
+          <img
+            src={sertifikat_dicoding_membuat_frontend_web_untuk_pemula}
+            alt="sertifikat_dicoding_membuat_frontend_web_untuk_pemula"
+          />
         </ul>
         <ul>
           <h3>2022-06</h3>
           <li>Belajar Dasar Pemrograman Web.</li>
+          <img
+            src={sertifikat_dicoding_belajar_dasar_pemrogtraman_web}
+            alt="sertifikat_dicoding_belajar_dasar_pemrogtraman_web"
+          />
         </ul>
       </div>
     </div>
