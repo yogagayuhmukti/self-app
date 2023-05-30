ffe18f5e1e4b154ca14d5ab512401e051e33c9d8
gm906~ <gayuhmukti906@gmail.com>
2023-05-30 21:30:29 +0700

    worksotry-fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,16 +9,16 @@ import instagram from "./instagram.png";
 import whatsapp from "./whatsapp.png";
 import yogabinaruser from "./yogabinaruser.jpg";
 import yogabinaradmin from "./yogabinaradmin.jpg";
-// import sertifikat_binar_academy from "./sertifikat_binar_academy.png";
-// import sertifikat_dicoding_belajar_dasar_aws_cloud from "./sertifikat_dicoding_belajar_dasar_aws_cloud.png";
-// import sertifikat_dicoding_belajar_dasar_dasar_dev_ops from "./sertifikat_dicoding_belajar_dasar_dasar_dev_ops.png";
-// import sertifikat_dicoding_belajar_dasar_pemrograman_javascript from "./sertifikat_dicoding_belajar_dasar_pemrograman_javascript.png";
-// import sertifikat_dicoding_belajar_dasar_pemrogtraman_web from "./sertifikat_dicoding_belajar_dasar_pemrogtraman_web.png";
-// import sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio from "./sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio.png";
-// import sertifikat_dicoding_membuat_frontend_web_untuk_pemula from "./sertifikat_dicoding_membuat_frontend_web_untuk_pemula.png";
-// import sertifikat_freecodecamp from "./sertifikat_freecodecamp.png";
-// import Sertifikat_Bahasa_Inggris from "./Sertifikat_Bahasa_Inggris.png";
-// import Sertifikat_Bahasa_Korea from "./Sertifikat_Bahasa_Korea.png";
+import sertifikat_binar_academy from "./sertifikat_binar_academy.png";
+import sertifikat_dicoding_belajar_dasar_aws_cloud from "./sertifikat_dicoding_belajar_dasar_aws_cloud.png";
+import sertifikat_dicoding_belajar_dasar_dasar_dev_ops from "./sertifikat_dicoding_belajar_dasar_dasar_dev_ops.png";
+import sertifikat_dicoding_belajar_dasar_pemrograman_javascript from "./sertifikat_dicoding_belajar_dasar_pemrograman_javascript.png";
+import sertifikat_dicoding_belajar_dasar_pemrogtraman_web from "./sertifikat_dicoding_belajar_dasar_pemrogtraman_web.png";
+import sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio from "./sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio.png";
+import sertifikat_dicoding_membuat_frontend_web_untuk_pemula from "./sertifikat_dicoding_membuat_frontend_web_untuk_pemula.png";
+import sertifikat_freecodecamp from "./sertifikat_freecodecamp.png";
+import Sertifikat_Bahasa_Inggris from "./Sertifikat_Bahasa_Inggris.png";
+import Sertifikat_Bahasa_Korea from "./Sertifikat_Bahasa_Korea.png";
 
 export {
   welcome,
@@ -32,14 +32,14 @@ export {
   whatsapp,
   yogabinaruser,
   yogabinaradmin,
-  // sertifikat_binar_academy,
-  // sertifikat_dicoding_belajar_dasar_aws_cloud,
-  // sertifikat_dicoding_belajar_dasar_dasar_dev_ops,
-  // sertifikat_dicoding_belajar_dasar_pemrograman_javascript,
-  // sertifikat_dicoding_belajar_dasar_pemrogtraman_web,
-  // sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio,
-  // sertifikat_dicoding_membuat_frontend_web_untuk_pemula,
-  // sertifikat_freecodecamp,
-  // Sertifikat_Bahasa_Inggris,
-  // Sertifikat_Bahasa_Korea,
+  sertifikat_binar_academy,
+  sertifikat_dicoding_belajar_dasar_aws_cloud,
+  sertifikat_dicoding_belajar_dasar_dasar_dev_ops,
+  sertifikat_dicoding_belajar_dasar_pemrograman_javascript,
+  sertifikat_dicoding_belajar_dasar_pemrogtraman_web,
+  sertifikat_dicoding_belajar_membuat_augmented_reality_menggunakan_lens_studio,
+  sertifikat_dicoding_membuat_frontend_web_untuk_pemula,
+  sertifikat_freecodecamp,
+  Sertifikat_Bahasa_Inggris,
+  Sertifikat_Bahasa_Korea,
 };
